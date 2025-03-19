43e006217f377babe28f12cb8bd02899b6997909
DEVA <divyanshuchaudhary482@gmail.com>
2025-03-20 01:59:53 +0530

    Update extraction.py

--- TFWMUSIC/utils/extraction.py
+++ TFWMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from TFWMUSICs import app
+from TFWMUSIC import app
 
 
 async def extract_user(m: Message) -> User:
